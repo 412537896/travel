a6c30d1469b74e41a69ee0da4e1bf4ad8f55da83
weiminw <weiminw80105@gmail.com>
2014-01-07 22:36:02 +0800

    updated codes

---
+++ travel/business/src/main/java/com/weiminw/business/aos/User.java
@@ -0,0 +1,140 @@
+package com.weiminw.business.aos;
+
+import com.weiminw.travel.interfaces.IUser;
+
+public class User implements IUser {
+	private final long id;
+	private final String name ;
+	private final String cellPhone;
+	private final String fixTelephone;
+	private final String cloudPushCk;
+	private final String cloudPushUk;
+	private final String cloudPushTag;
+	
+	public static final class Builder {
+		private final long id;
+		private String name = "";
+		private String cellPhone = "";
+		private String fixTelephone = "";
+		private String cloudPushCk = "";
+		private String cloudPushUk = "";
+		private String cloudPushTag = "";
+		public Builder(long id){
+			this.id = id;
+		}
+		public IUser build() {
+			// TODO Auto-generated method stub
+			return new User(this);
+		}
+		public String getName() {
+			return name;
+		}
+		public Builder setName(String name) {
+			if(name!=null)
+				this.name = name;
+			return this;
+		}
+		public String getCellPhone() {
+			return cellPhone;
+		}
+		public Builder setCellPhone(String cellPhone) {
+			if(cellPhone != null)
+				this.cellPhone = cellPhone;
+			return this;
+		}
+		public String getFixTelephone() {
+			return fixTelephone;
+		}
+		public Builder setFixTelephone(String fixTelephone) {
+			if(fixTelephone != null)
+				this.fixTelephone = fixTelephone;
+			return this;
+		}
+		public String getCloudPushCk() {
+			return cloudPushCk;
+		}
+		public Builder setCloudPushCk(String cloudPushCk) {
+			if(cloudPushCk != null)
+				this.cloudPushCk = cloudPushCk;
+			return this;
+		}
+		public String getCloudPushUk() {
+			return cloudPushUk;
+		}
+		public Builder setCloudPushUk(String cloudPushUk) {
+			this.cloudPushUk = cloudPushUk;
+			return this;
+		}
+		public String getCloudPushTag() {
+			return cloudPushTag;
+		}
+		public Builder setCloudPushTag(String cloudPushTag) {
+			this.cloudPushTag = cloudPushTag;
+			return this;
+		}
+		public long getId() {
+			return id;
+		}
+		
+		
+	}
+	
+	private User(Builder builder){
+		this.id = builder.id;
+		this.name = builder.name;
+		this.fixTelephone = builder.fixTelephone;
+		this.cellPhone = builder.cellPhone;
+		this.cloudPushCk = builder.cloudPushCk;
+		this.cloudPushUk = builder.cloudPushUk;
+		this.cloudPushTag = builder.cloudPushTag;
+	}
+	
+	public static Builder builder(){
+		return new Builder(0L);
+	}
+	public static Builder builder(long id){
+		return new Builder(id);
+	}
+	@Override
+	public long getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCellPhone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCloudPushCk() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCloudPushTag() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCloudPushUk() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFixTelephone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- travel/web/src/main/java/com/weiminw/web/services/SellerResource.java
+++ travel/web/src/main/java/com/weiminw/web/services/SellerResource.java
@@ -1,28 +1,30 @@
 package com.weiminw.web.services;
 
 import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import com.weiminw.business.aos.Seller;
-import com.weiminw.business.aos.User;
 import com.weiminw.business.managers.UserManager;
 import com.weiminw.travel.interfaces.IUser;
 import com.weiminw.travel.interfaces.IUserManager;
-@Path("/users")
-public class UserResource {
+
+@Path("/sellers")
+public class SellerResource {
 	IUserManager manager = UserManager.create();
 	@Context
 	UriInfo uriInfo;
+	@POST
 	public Response createUser(@FormParam(value = "name") String name,
 			@FormParam(value="cellPhone") String cellPhone,
 			@FormParam(value="cloudPushCk") String cloudPushCk,
 			@FormParam(value="cloudPushUk") String cloudPushUk,
 			@FormParam(value="cloudPushTag") String cloudPushTag,
 			@FormParam(value="fixTelephone") String fixTelephone){
-		IUser user = User.builder()
+		IUser user = Seller.builder()
 				.setName(name)
 				.setCellPhone(cellPhone)
 				.setCloudPushCk(cloudPushCk)
